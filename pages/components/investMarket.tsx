eb08ca8b93ba42e4c2f9948570942930ee1ce064
Daurius Swarts <ali@openmindi.co.za>
2023-10-31 21:50:58 +0200

    [FE] Fixed responsiveness

--- pages/components/investMarket.tsx
+++ pages/components/investMarket.tsx
@@ -8,38 +8,38 @@ export default function InvestMarket() {
         <p className="text-center text-sm mb-4 font-semibold">
           Investment Marketplace
         </p>
-        <div className="flex flex-wrap items-center justify-between gap-2">
-          <div className="flex flex-col items-center bg-slate-100 rounded-lg p-4 w-36 transform hover:scale-105 transition-transform ease-in-out duration-300 hover:border-b-2 hover:border-primary-100 hover:text-primary-100">
+        <div className="grid grid-cols-3 gap-2">
+          <div className="flex flex-col items-center bg-slate-100 rounded-lg p-2  transform hover:scale-105 transition-transform ease-in-out duration-300 hover:border-b-2 hover:border-primary-100 hover:text-primary-100">
             <i className="fa-solid fa-tractor text-2xl"></i>
             <p className="p-2 text-sm">
               Agriculture
             </p>
           </div>
-          <div className="flex flex-col items-center bg-slate-100 rounded-lg p-4 w-36 transform hover:scale-105 transition-transform ease-in-out duration-300 hover:border-b-2 hover:border-primary-100 hover:text-primary-100">
+          <div className="flex flex-col items-center bg-slate-100 rounded-lg p-2  transform hover:scale-105 transition-transform ease-in-out duration-300 hover:border-b-2 hover:border-primary-100 hover:text-primary-100">
             <i className="fa-solid fa-sign-hanging text-2xl"></i>
             <p className="p-2 text-sm">
               Real Estate
             </p>
           </div>
-          <div className="flex flex-col items-center bg-slate-100 rounded-lg p-4 w-36 transform hover:scale-105 transition-transform ease-in-out duration-300 hover:border-b-2 hover:border-primary-100 hover:text-primary-100">
+          <div className="flex flex-col items-center bg-slate-100 rounded-lg p-2  transform hover:scale-105 transition-transform ease-in-out duration-300 hover:border-b-2 hover:border-primary-100 hover:text-primary-100">
             <i className="fa-solid fa-truck-plane text-2xl"></i>
             <p className="p-2 text-sm">
               Transport
             </p>
           </div>
-          <div className="flex flex-col items-center bg-slate-100 rounded-lg p-4 w-36 transform hover:scale-105 transition-transform ease-in-out duration-300 hover:border-b-2 hover:border-primary-100 hover:text-primary-100">
+          <div className="flex flex-col items-center bg-slate-100 rounded-lg p-2  transform hover:scale-105 transition-transform ease-in-out duration-300 hover:border-b-2 hover:border-primary-100 hover:text-primary-100">
             <i className="fa-solid fa-book-open-reader text-2xl"></i>
             <p className="p-2 text-sm">
               Education
             </p>
           </div>
-          <div className="flex flex-col items-center bg-slate-100 rounded-lg p-4 w-36 transform hover:scale-105 transition-transform ease-in-out duration-300 hover:border-b-2 hover:border-primary-100 hover:text-primary-100">
+          <div className="flex flex-col items-center bg-slate-100 rounded-lg p-2  transform hover:scale-105 transition-transform ease-in-out duration-300 hover:border-b-2 hover:border-primary-100 hover:text-primary-100">
             <i className="fa-solid fa-microchip text-2xl "></i>
             <p className="p-2 text-sm">
               Technology
             </p>
           </div>
-          <div className="flex flex-col items-center bg-slate-100 rounded-lg p-4 w-36 transform hover:scale-105 transition-transform ease-in-out duration-300 hover:border-b-2 hover:border-primary-100 hover:text-primary-100">
+          <div className="flex flex-col items-center bg-slate-100 rounded-lg p-2  transform hover:scale-105 transition-transform ease-in-out duration-300 hover:border-b-2 hover:border-primary-100 hover:text-primary-100">
             <i className="fa-solid fa-bolt text-2xl"></i>
             <p className="p-2 text-sm">Energy</p>
           </div>
--- pages/components/quickService.tsx
+++ pages/components/quickService.tsx
@@ -10,19 +10,19 @@ export default function QuickService() {
           Quick Services
         </p>
 
-        <div className="flex items-center justify-between w-[80%] mx-auto">
+        <div className="flex items-center justify-between lg:w-[80%] mx-auto">
           <Link href="/transact">
-            <div className="flex flex-col items-center bg-slate-100 rounded-lg py-4 px-8  transform hover:scale-110 transition-transform ease-in-out duration-300 hover:border-b-2 hover:border-primary-100 hover:text-primary-100">
+            <div className="flex flex-col items-center bg-slate-100 rounded-lg py-4 px-6 transform hover:scale-110 transition-transform ease-in-out duration-300 hover:border-b-2 hover:border-primary-100 hover:text-primary-100">
               <i className="fa-solid fa-money-bill-trend-up text-2xl"></i>
               <p className="p-2 text-sm">Send</p>
             </div>
           </Link>
           <Link href="/invest">
-          <div className="flex flex-col items-center bg-slate-100 rounded-lg py-4 px-8  transform hover:scale-110  transition-transform ease-in-out duration-300 hover:border-b-2  hover:border-primary-100 hover:text-primary-100">
+          <div className="flex flex-col items-center bg-slate-100 rounded-lg py-4 px-6 transform hover:scale-110  transition-transform ease-in-out duration-300 hover:border-b-2  hover:border-primary-100 hover:text-primary-100">
             <i className="fa-solid fa-piggy-bank text-2xl "></i>
             <p className="p-2 text-sm">Invest</p>
           </div></Link>
-          <div className="flex flex-col items-center bg-slate-100 rounded-lg py-4 px-8  transform hover:scale-110  transition-transform ease-in-out duration-300 hover:border-b-2  hover:border-primary-100 hover:text-primary-100">
+          <div className="flex flex-col items-center bg-slate-100 rounded-lg py-4 px-6 transform hover:scale-110  transition-transform ease-in-out duration-300 hover:border-b-2  hover:border-primary-100 hover:text-primary-100">
             <i className="fa-solid fa-file-invoice text-2xl"></i>
             <p className="p-2 text-sm">History</p>
           </div>
